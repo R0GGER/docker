9e658606cab221bde2f34f8d4df80a9285b99c00
R0GGER <rogger@spotx.nu>
2015-10-23 17:09:36 +0200

    Create setup.sh

---
+++ wsus/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# CREDITS: Paul Liljenberg <liljenberg.paul@gmail.com>
+
+SYSTEM=${SYSTEM:-"all-x64"}
+OFFICE=${OFFICE:-"ofc"}
+LANGUAGE=${LANGUAGE:-"enu"}
+PARAMS=${PARAMS:-"/msse /dotnet /wddefs /wle"}
+SLEEP=${SLEEP:-"48h"}
+
+ctrlc()
+{
+    exit 1   # stop the script
+}
+
+trap 'ctrlc' INT
+
+while true; do
+    cd /wsusoffline/sh && ./DownloadUpdates.sh "$SYSTEM" "$LANGUAGE" "$PARAMS"
+	cd /wsusoffline/sh && ./DownloadUpdates.sh "$OFFICE" "$LANGUAGE" 
+    echo "Sleeping for $SLEEP before next download."
+    sleep "$SLEEP"
+done
